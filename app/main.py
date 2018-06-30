914be991a9f352ad91ffa38359d435dabea7de2b
Petar Grubac <pgrubacc@gmail.com>
2018-06-30 16:15:17 +0200

    Random number instead of hello world

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import random
 
 # the all-important app variable:
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Oh, Hello World"
+    return "This is a random number {0} \
+           from 1 to 10".format(random.randint(1,10))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8080)
