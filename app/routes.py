2ede2e6cb2f3f6f2b13730f0dcc9b3f8a4ccfb4b
Petar Grubac <pgrubacc@gmail.com>
2018-07-04 21:29:44 +0200

    Profile page and Gravatar avatars added

--- app/routes.py
+++ app/routes.py
@@ -46,6 +46,7 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -59,3 +60,14 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
